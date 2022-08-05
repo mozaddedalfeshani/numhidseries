a777954d28d121eeef66ffc53e6a39feaff2d18e
Macbook Air <Murad@macbooks-MacBook-Air.local>
2022-08-05 14:30:47 +0600

    update differenc

--- series hider.cpp
+++ series hider.cpp
@@ -9,25 +9,41 @@ int main()
     short int i, in;
     short int last_value, temo, temp;
     short int arrayvaluetemp;
-    int temp3;
+    int temp3, difference;
 
     int start, arraydtector;
 
 again:
+
     cout << " Work in progress ! Don't upload any number which little than initial value & minus sign" << endl;
+    cout << "Welcome to Muradian number series hider" << endl;
 
     cout << "Enter what is the initial value" << endl;
     cin >> in;
 
+    // detect last value is elder than initial value
+
+last_value:
+
     cout << "What is the last value ? " << endl;
     cin >> last_value;
+    if (last_value < in)
+    {
+        cout << "Never Input small number than initial number" << endl;
+        goto last_value;
+    }
 
 input:
     cout << "How Many Number You want to remove from series ?"
          << endl;
     cin >> userarraysize;
     int array[userarraysize + 1];
-    if (userarraysize > last_value || userarraysize < in)
+
+    // out of range detector
+    difference = last_value - in;
+
+    // wrong detector
+    if (difference < userarraysize)
     {
         cout << "Enter Correct Value" << endl;
         goto input;
@@ -40,6 +56,7 @@ input:
     for (short int input = 1; input <= userarraysize;)
     {
         cin >> temp3;
+
         if (temp3 >= in && temp3 <= last_value)
         {
             array[input] = temp3;
